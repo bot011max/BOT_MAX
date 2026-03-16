6385078ca2d1f2836bd8025c3e4094286d191798
bot011max <bot011max@gmail.com>
2026-03-16 21:04:30 +0300

    Update main.go

--- cmd/telegram-bot/main.go
+++ cmd/telegram-bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "os"
     "os/signal"
@@ -24,15 +25,52 @@ func main() {
         log.Fatal("TELEGRAM_TOKEN не задан")
     }
     
+    // Получаем параметры БД из окружения (или используем значения по умолчанию)
+    dbHost := os.Getenv("DB_HOST")
+    if dbHost == "" {
+        dbHost = "localhost"
+    }
+    
+    dbPort := os.Getenv("DB_PORT")
+    if dbPort == "" {
+        dbPort = "5432"
+    }
+    
+    dbUser := os.Getenv("DB_USER")
+    if dbUser == "" {
+        dbUser = "postgres"
+    }
+    
+    dbPassword := os.Getenv("DB_PASSWORD")
+    if dbPassword == "" {
+        dbPassword = "postgres"
+    }
+    
+    dbName := os.Getenv("DB_NAME")
+    if dbName == "" {
+        dbName = "medical_bot"
+    }
+    
+    // Формируем строку подключения с переменными
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        dbHost, dbUser, dbPassword, dbName, dbPort)
+    
+    log.Printf("Подключение к БД: host=%s dbname=%s", dbHost, dbName)
+    
     // Подключаемся к БД
-    dsn := "host=localhost user=postgres password=postgres dbname=medical_bot port=5432 sslmode=disable"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Ошибка подключения к БД:", err)
     }
     
+    log.Println("База данных успешно подключена")
+    
     // Автоматическая миграция
-    db.AutoMigrate(&telegram.TelegramUser{}, &telegram.TelegramSession{}, &telegram.Reminder{})
+    if err := db.AutoMigrate(&telegram.TelegramUser{}, &telegram.TelegramSession{}, &telegram.Reminder{}); err != nil {
+        log.Fatal("Ошибка миграции:", err)
+    }
+    
+    log.Println("Миграции успешно применены")
     
     // Создаем бота
     bot, err := telegram.NewTelegramBot(token, db)
@@ -40,7 +78,7 @@ func main() {
         log.Fatal("Ошибка создания бота:", err)
     }
     
-    // Запускаем в polling-режиме (проще для разработки)
+    // Запускаем в polling-режиме
     log.Println("Бот запущен в polling-режиме")
     go bot.StartPolling()
     
